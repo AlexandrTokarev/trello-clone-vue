5d8709d6402a63b2265f85047ed03bb8e9a9f1bc
Alexandr Tokarev <46864005+AlexandrTokarev@users.noreply.github.com>
2026-02-28 13:20:45 +0600

    feat: add some css vars, button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import UiButton from '@/shared/ui/UiButton/UiButton.vue';
+</script>
 
 <template>
-  <div>App Trello</div>
+  <div>
+    <h5>App Trello</h5>
+    <UiButton>UI button</UiButton>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/shared/ui/UiButton/UiButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button :class="$style.button">
+    <slot />
+  </button>
+</template>
+
+<style module>
+.button {
+  background: var(--color-primary);
+  color: var(--color-text-main);
+  border: none;
+  padding: var(--space-2) var(--space-4);
+  border-radius: var(--radius-md);
+  cursor: pointer;
+  transition: var(--transition);
+
+  &:hover {
+    background: var(--color-primary-hover);
+  }
+}
+</style>
